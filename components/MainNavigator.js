87170245fbbe2e2efcea312cc5a2d007b839410b
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 18:20:58 -0400

    feat: MainNavigator - add other Screens
    Add screens in MainNavigator that lead
    to Home, Deck details, Quiz details,
    and New question.

--- components/MainNavigator.js
+++ components/MainNavigator.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import NaviTab from './NaviTab'
+import DeckView from './DeckView';
+import QuizView from './QuizView';
+import NewQuestionView from './NewQuestionView';
 
 const Stack = createStackNavigator()
 
 const MainNavigator = () => (
     <Stack.Navigator>
-        
+        <Stack.Screen name = 'Home' component = {NaviTab}/>
+        <Stack.Screen name = 'Deck details' component = {DeckView}/>
+        <Stack.Screen name = 'Quiz details' component = {QuizView}/>
+        <Stack.Screen name = 'New question' component = {NewQuestionView}/>
     </Stack.Navigator>
 )
+export default MainNavigator
