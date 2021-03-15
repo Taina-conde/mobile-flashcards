26b8610fb5ff46f555c70feef4859bc9c0331450
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 16:16:40 -0400

    feat: Create NewDeckView minimal code

---
+++ components/NewDeckView.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import { View, Text, StyleSheet } from 'react-native'
+
+class NewDeckView extends React.Component {
+    render(){
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
+export default NewDeckView
