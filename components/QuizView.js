ffe977d475ea31bda6faa4de70c47207f1611c74
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 16:15:14 -0400

    feat: Create QuizView minimal code

---
+++ components/QuizView.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import { View, Text, StyleSheet } from 'react-native'
+
+class QuizView extends React.Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
+export default QuizView
