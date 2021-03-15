e28b9d19b34154966227332aebfe381dbe43f484
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 17:20:05 -0400

    feat: App - create tab navigation
    Create NaviTab with Decks and Add Deck
    screens. Customize tabBarIcon depending
    on the route.

--- App.js
+++ App.js
@@ -5,7 +5,27 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import DeckListView from './components/DeckListView';
+import NewDeckView from './components/NewDeckView';
+import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 
+const NaviTab = createBottomTabNavigator({
+  Decks: DeckListView,
+  'Add Deck': NewDeckView,
+
+}, {
+  defaultNavigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({tintColor}) => {
+      const { routeName } = navigation.state;
+      
+      if (routeName === 'Decks') {
+        return <MaterialCommunityIcons name="cards" size={24} color={tintColor} />
+      } else if (routeName === 'Add Deck') {
+        return <Entypo name="plus" size={24} color={tintColor} />
+      }
+    }
+  })
+})
 export default function App() {
   return (
     <View style={styles.container}>
