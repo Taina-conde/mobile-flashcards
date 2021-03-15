f242500746c3d6492ccec17c3d8775513ae09f11
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 18:12:02 -0400

    refactor: Move NaviTab from App to its own file
    Create NaviTab file, move the NaviTab
    related content from App to its file.
    The reason for this is to separate
    concerns and better organize the code.

---
+++ components/NaviTab.js
@@ -0,0 +1,25 @@
+import React from 'react' 
+import DeckListView from './components/DeckListView';
+import NewDeckView from './components/NewDeckView';
+import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const NaviTab = () => (
+  <Tab.Navigator
+    screenOptions = { ({ route }) => ({
+      tabBarIcon: ({ color }) => {
+        if (route.name === 'Decks') {
+          return <MaterialCommunityIcons name="cards" size={24} color={color} />
+        } 
+        return <Entypo name="plus" size={24} color={color} />
+      }
+    })}
+  >
+    <Tab.Screen name = 'Decks' component = {DeckListView}/>
+
+    <Tab.Screen name = 'Add Deck' component = {NewDeckView}/>
+  </Tab.Navigator>
+)
+export default NaviTab
