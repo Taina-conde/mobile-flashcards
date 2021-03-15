b67530bd57ba2518419ed665e25294b88ce899a5
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 16:09:55 -0400

    feat: Create components folder and DeckListView
    Create DeckListView file and minimal
    starter code.

---
+++ components/DeckListView.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import { View, Text, StyleSheet } from 'react-native'
+
+class DeckListView extends React.Component {
+    render (){
+        return(
+            <View>
+
+            </View>
+        )
+    }
+}
+export default DeckListView
