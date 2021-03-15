397bff7f965ef0e8fcf10785b27d1a389e5d90c3
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 18:22:27 -0400

    feat: App- import and render MainNavigator

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './components/MainNavigator';
 
 
 
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer >
       <StatusBar style="auto" />
-      
+      <MainNavigator/>
     </NavigationContainer>
   );
 }
