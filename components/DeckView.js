5467f01da0ad088323221d243d4998c468ffbf44
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 16:12:54 -0400

    feat: Create DeckView bare-bone component

---
+++ components/DeckView.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import { View, Text, StyleSheet } from 'react-native'
+
+class DeckView extends React.Component {
+    render(){
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
+export default DeckView
