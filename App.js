259475df71753caaab8460e3c02bafb4a1b98090
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 16:53:24 -0400

    feat: Install react-navigation packages
    Import createAppContainer, createStackNavigator
    and createBottomTabNavigator in App.js.

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 export default function App() {
   return (
