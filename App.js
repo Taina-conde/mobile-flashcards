f501bb85c9e7a20e278e43979dcda916bf5230a7
Taina Conde <tainabatistaconde@gmail.com>
2021-03-15 17:50:00 -0400

    feat: Upgrade react-navigation
    Upgrade package to version 5 and fix
    imports to match the new version.

--- App.js
+++ App.js
@@ -2,30 +2,14 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import DeckListView from './components/DeckListView';
 import NewDeckView from './components/NewDeckView';
 import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 
-const NaviTab = createBottomTabNavigator({
-  Decks: DeckListView,
-  'Add Deck': NewDeckView,
-
-}, {
-  defaultNavigationOptions: ({ navigation }) => ({
-    tabBarIcon: ({tintColor}) => {
-      const { routeName } = navigation.state;
-      
-      if (routeName === 'Decks') {
-        return <MaterialCommunityIcons name="cards" size={24} color={tintColor} />
-      } else if (routeName === 'Add Deck') {
-        return <Entypo name="plus" size={24} color={tintColor} />
-      }
-    }
-  })
-})
+const NaviTab = createBottomTabNavigator()
 export default function App() {
   return (
     <View style={styles.container}>
