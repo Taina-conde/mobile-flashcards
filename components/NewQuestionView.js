2489782b31795ff874730ded4c99825db3617df2
Taina Conde <tainabatistaconde@gmail.com>
2021-03-16 18:46:15 -0400

    feat: NewQuestionView - add TextInputs and submit button

--- components/NewQuestionView.js
+++ components/NewQuestionView.js
@@ -1,11 +1,26 @@
 import React from 'react' 
-import { View, Text, StyleSheet } from 'react-native'
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput,
+    TouchableOpacity,
+ } from 'react-native'
 
 class NewQuestionView extends React.Component {
+    state = {
+        question: '',
+        answer: ''
+    }
     render(){
         return (
             <View>
                 <Text>NewQuestionView</Text>
+                <TextInput/>
+                <TextInput/>
+                <TouchableOpacity>
+                    SUBMIT
+                </TouchableOpacity>
             </View>
         )
     }
