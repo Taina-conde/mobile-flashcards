88f1d87c3c4680b2a3567f83395139238b65c11d
Taina Conde <tainabatistaconde@gmail.com>
2021-03-16 17:10:44 -0400

    feat: NewDeckView - add TextInput
    Implement TextInput with handler to
    handle onChangeText. Use components
    state to handle input change.

--- components/NewDeckView.js
+++ components/NewDeckView.js
@@ -1,11 +1,26 @@
 import React from 'react' 
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 class NewDeckView extends React.Component {
+    state = {
+        input: ""
+    }
+    handleTextChange = (input) => {
+        this.setState(()=> ({
+            input
+        }))
+    }
     render(){
         return (
             <View>
                 <Text>NewDeckView</Text>
+                <Text>What is the title of your new deck?</Text>
+                <TextInput
+                    onChangeText = {this.handleTextChange}
+                    value= {this.state.input}
+                    placeholder = "Deck title"
+                />
+               
             </View>
         )
     }
