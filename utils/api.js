4328301189f13cffe22415a3239e2aa388cae9a4
Taina Conde <tainabatistaconde@gmail.com>
2021-03-16 16:56:00 -0400

    feat: Api - set up AsyncStorage
    Set a key to store decks to an empty
    obj. Build up getDecks helper function.

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,13 @@
 import { AsyncStorage } from 'react-native' 
 
+const DECKS_STORAGE_KEY = '@Decks'
+
+AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify({}))
+
+export function getDecks() {
+    return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+        .then(JSON.parse(results))
+}
+
+
 
