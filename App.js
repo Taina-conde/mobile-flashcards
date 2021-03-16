56ffc755a006775c4a740d10c97bb6c6e71ae47d
Taina Conde <tainabatistaconde@gmail.com>
2021-03-16 14:13:10 -0400

    feat: Install redux and react-redux
    In App, import createStore, Provider and
    reducer. Wrap App in Provider passing it
    the store.

--- App.js
+++ App.js
@@ -4,16 +4,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './components/MainNavigator';
-
+import { createStore } from 'redux' 
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 
 
 export default function App() {
   return (
-    <NavigationContainer >
-      <StatusBar style="auto" />
-      <MainNavigator/>
-    </NavigationContainer>
+    <Provider store = {createStore(reducer)}>
+      <NavigationContainer >
+        <StatusBar style="auto" />
+        <MainNavigator/>
+      </NavigationContainer>
+    </Provider>
+    
   );
 }
 
