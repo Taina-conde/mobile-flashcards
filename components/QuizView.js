837e5013e0d50e64c2ca3a3198aabad9283d2078
Taina Conde <tainabatistaconde@gmail.com>
2021-03-17 14:27:46 -0400

    feat: QuizView - return NoCards
    If user tries to start a quiz in an empty
    deck, render the NoCards component with
    a message to the user.

--- components/QuizView.js
+++ components/QuizView.js
@@ -1,13 +1,13 @@
 import React from 'react' 
 import { View, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
-
+import NoCards from './NoCards'
 class QuizView extends React.Component {
     render() {
         const { route, decks } = this.props;
         const { deckId } = route.params;
-        if (decks[deckId] === "undefined") {
-            
+        if (decks[deckId].questions.length === 0) {
+            return <NoCards/>
         }
         return (
             <View>
