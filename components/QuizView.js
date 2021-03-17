0506239b8c430ed994e0bbe424c15f8262234350
Taina Conde <tainabatistaconde@gmail.com>
2021-03-17 15:32:03 -0400

    feat: QuizView - start quiz
    Questions is an array, so the plan is to
    start to show the first item in the array
    in index 0, and when user clicks correct
    or incorrect, the currentCardIndex will
    be added 1 and the next card will render.

--- components/QuizView.js
+++ components/QuizView.js
@@ -1,18 +1,28 @@
 import React from 'react' 
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import NoCards from './NoCards'
 class QuizView extends React.Component {
     render() {
         const { route, decks } = this.props;
         const { deckId } = route.params;
-        if (decks[deckId].questions.length === 0) {
+        const deck = decks[deckId];
+        if (deck.questions.length === 0) {
             return <NoCards/>
         }
+        const cardsTotal = deck.questions.length;
+        let currentCardIndex = 0;
+        let currentCard = deck.questions[currentCardIndex];
         return (
             <View>
                 <Text>QuizView</Text>
-                
+                <Text>{`${currentCardIndex + 1}/${cardsTotal}`}</Text>
+                <View>
+                    <Text>{currentCard.question}</Text>
+                    <TouchableOpacity>
+                        <Text>View answer</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
