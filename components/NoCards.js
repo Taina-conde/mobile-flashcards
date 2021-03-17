85a9a4875879063340895d26d45bc98a7a6eb90e
Taina Conde <tainabatistaconde@gmail.com>
2021-03-17 14:24:07 -0400

    feat: Create NoCards component
    Add new file for NoCards component. Return
    a message in case the user tries to start
    the quiz in an empty deck.

---
+++ components/NoCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const NoCards = () => (
+    <Text>
+        Sorry, there are no cards in this deck. 
+        Please, select another deck to start 
+        the quiz or add cards to this deck.
+    </Text>
+    
+)
+export default NoCards;
