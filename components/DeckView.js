2823ae27a5e25d57dc12ea92f039aa7b68fbb09d
Taina Conde <tainabatistaconde@gmail.com>
2021-03-18 15:58:19 -0400

    style: DeckView - create styles

--- components/DeckView.js
+++ components/DeckView.js
@@ -8,21 +8,65 @@ class DeckView extends React.Component {
         const { deckId } = route.params
         
         return (
-            <View>
-                <Text>DeckView</Text>
-                <Text>{deckId}</Text>
-                <Text>Number of cards: {decks[deckId].questions.length}</Text>
+            <View style = {styles.container}>
                 
-                <TouchableOpacity onPress = {() => navigation.navigate('New question', { deckId})}>
-                    <Text>Add card</Text>
+                <Text style = {styles.deckTitle}>{deckId}</Text>
+                <Text style = {styles.numCards}>Number of cards: {decks[deckId].questions.length}</Text>
+                
+                <TouchableOpacity 
+                    style = {styles.addBtn}
+                    onPress = {() => navigation.navigate(
+                        'New question', 
+                        { deckId })}>
+                    <Text style = {styles.buttonText}>Add card</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress = {() => navigation.navigate('Quiz details', { deckId })}>
+                <TouchableOpacity 
+                    style = {styles.startBtn}
+                    onPress = {() => navigation.navigate(
+                        'Quiz details', 
+                        { deckId })}>
                     <Text>Start quiz</Text>
                 </TouchableOpacity>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    deckTitle : {
+        fontSize: 46,
+        color: '#084B8A',
+    },
+    numCards : {
+        fontSize: 22, 
+        color: '#848484',
+    },
+    addBtn : {
+        padding: 20,
+        margin: 20,
+        backgroundColor: '#01A9DB',
+        borderRadius: 30,
+        shadowColor: `rgba(0,0,0, 0.24)`,
+        shadowOffset: {
+            width: 0, 
+            height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+        
+    },
+    buttonText : {
+        color: '#ffffff',
+        fontSize: 16,
+        textTransform: 'uppercase',
+        fontWeight: 'bold'
+    }
+})
 function mapStateToProps(decks) {
     return {
         decks,
