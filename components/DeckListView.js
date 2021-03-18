88af5e5bf2efb7f6682d8c8e9f1fe30958497179
Taina Conde <tainabatistaconde@gmail.com>
2021-03-18 14:10:23 -0400

    style: DeckListView - deckBox, deckTitle and numCards

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -20,9 +20,9 @@ class DeckListView extends React.Component {
         const {navigation} = this.props
         console.log('renderItem', item)
         return (
-            <TouchableOpacity onPress = {() => navigation.navigate('Deck details', { deckId: item.title})}>
-                <Text>{item.title }</Text>
-                <Text>{item.questions.length}</Text>
+            <TouchableOpacity style= {styles.deckBox} onPress = {() => navigation.navigate('Deck details', { deckId: item.title})}>
+                <Text style = {styles.deckTitle}>{item.title }</Text>
+                <Text style = {styles.numCards}>{item.questions.length} cards</Text>
             </TouchableOpacity>
         )
     }
@@ -32,10 +32,11 @@ class DeckListView extends React.Component {
         const data = Object.values(decks)
         return(
             <View style = {styles.container}>
-                <Text>DeckListView</Text>
+                
                 { (decksKeys.length === 0)
-                  ? <Text> 0 decks</Text>
+                  ? <Text style = {styles.deckBox}> 0 decks</Text>
                   : <FlatList 
+                        
                         data = {data}
                         renderItem = {this.renderItem}
                         keyExtractor = { item => item.title}
@@ -53,8 +54,35 @@ const styles = StyleSheet.create({
     container : {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center', 
+        alignItems: 'stretch', 
+    },
+    deckBox : {
+        height: 100,
+        margin: 20,
+        backgroundColor: '#ffff',
+        padding: 20,
+        shadowColor: `rgba(0,0,0, 0.24)`,
+          shadowOffset: {
+            width: 0, 
+            height: 3,
+          },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+        textAlign: 'center',
+        borderRadius: 10,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    deckTitle : {
+        fontSize: 22,
+        marginBottom: 5,
+    },
+    numCards : {
+        color: '#848484',
     }
+
+    
 })
 
 function mapStateToProps(decks) {
