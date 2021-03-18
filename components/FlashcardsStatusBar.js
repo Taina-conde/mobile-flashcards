a367645be0d0bdeb0c64797fc5244acbbf974f4e
Taina Conde <tainabatistaconde@gmail.com>
2021-03-18 15:06:09 -0400

    feat: Create FlashcardsStatusBar
    Create the component to render the status
    bar. Separate it from the App component
    to make the code better to read and
    underestand.

---
+++ components/FlashcardsStatusBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, StatusBar, StyleSheet} from 'react-native'
+
+const FlashcardsStatusBar = () => (
+    <View>
+        <StatusBar/>
+    </View>
+)
+
+export default FlashcardsStatusBar;
