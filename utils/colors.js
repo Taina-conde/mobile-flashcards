76b43c6fca1ab9ad0e66b1d21038eb4cc893f04b
Taina Conde <tainabatistaconde@gmail.com>
2021-03-19 10:14:03 -0400

    feat: Create colors file
    Create this file exporting the colors that
    I used so far to help with future styling.

---
+++ utils/colors.js
@@ -0,0 +1,6 @@
+export const white = '#fff';
+export const fadedWhite = 'rgba(255, 255, 255, 0.7)';
+export const blue = '#084B8A';
+export const lightBlue = '#01A9DB';
+export const fadedGreen = "rgba(4, 180, 134, 0.4)";
+export const green = '#04B486';
