6643a307cd6eb761bf3f46c98955b9cb8d860849
Taina Conde <tainabatistaconde@gmail.com>
2021-03-24 17:04:48 -0400

    feat: NoCards - add warning icon
    Add warning icon from expo vector-icons.
    Make font weight normal and centralized.

--- components/NoCards.js
+++ components/NoCards.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View} from 'react-native';
 import { lightGray } from '../utils/colors'
+import { FontAwesome } from '@expo/vector-icons';
 
 const NoCards = () => (
     <View style = {styles.container}>
+        <FontAwesome name="warning" size={70} color="black" />
         <Text style = {styles.noCardsText}>
             Sorry, there are no cards in this deck. 
             Please, select another deck to start 
@@ -22,9 +24,9 @@ const styles = StyleSheet.create({
         backgroundColor: lightGray,
     },
     noCardsText : {
-        fontSize: 22,
-        fontWeight: 'bold',
+        fontSize: 18,
         margin: 20,
+        textAlign: 'center'
     }
 })
 export default NoCards;
